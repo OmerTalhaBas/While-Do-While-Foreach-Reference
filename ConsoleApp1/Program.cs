4e5dee8003ce20509369980a3b2d3d7d452c27bb
201-5 <201-5@DESKTOP-I1JH03S>
2024-09-10 21:57:22 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region Şanslı Sayı
+            //bool a = true;
+            //Random ran = new Random();
+            //int tutulanSayi = ran.Next(1, 50);
+            //int girilenSayi = 0;
+            //do
+            //{
+            //    Console.WriteLine("Sayı Giriniz : ");
+            //    girilenSayi = int.Parse(Console.ReadLine());
+            //    if (girilenSayi > tutulanSayi)
+            //    {
+            //        Console.WriteLine("Daha Küçük Bir Değer Girin");
+            //    }
+            //    else if (girilenSayi < tutulanSayi)
+            //    {
+            //        Console.WriteLine("Daha Büyük Bir Sayı Giriniz");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Tebrikler Sayıyı Buldunuz. \n Tutulan Sayı = {0}", tutulanSayi);
+            //    }
+            //    if (tutulanSayi == girilenSayi)
+            //    {
+            //        a = false;
+            //    }
+            //} while (a);
+            //#endregion 
+            //#region ATM 
+            //int bakiye = 1000;
+            //int cekilecekMiktar;
+            //do
+            //{
+            //    Console.WriteLine("Çekmek İstediğiniz Miktarı Giriniz : | Bakiyeniz : {0}", bakiye);
+            //    cekilecekMiktar = int.Parse(Console.ReadLine());
+
+            //    if (cekilecekMiktar > bakiye)
+            //    {
+            //        Console.WriteLine("Yetersiz Bakiye!");
+            //    }
+            //} while (cekilecekMiktar > bakiye);
+            //bakiye -= cekilecekMiktar;
+            //Console.WriteLine("İşlem Başarılı Kalan Bakiyeniz | {0}", bakiye);
+            //#endregion
+            #region
+
+
+
+
+            #endregion
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////var komudu her şeyi içine alır
+            ////string[] ad = { "Kaan", "Özlem", "Leyla", "Burak", "Mustafa", }; //dizinin indexi 0 dan başlar yani erkan karakteri 4. indexe sahip kaan ise 0
+            ////for (int i = 0; i < ad.Length; i++)// döngüde her seferinde koşulu tek tek kontrol eder içindeki elemanları yazdırır
+            ////{
+            ////    Console.WriteLine(ad[i]);
+            ////}
+            ////Console.WriteLine("**********");
+            ////foreach (string ogrenci in ad)
+            ////{
+            ////    Console.WriteLine(ogrenci);
+            ////}
+            //int[] intDizi = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            //foreach (int sayi in intDizi)
+            //{
+            //    Console.WriteLine(sayi);
+            //}
+            //int[] intDizi = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            //foreach (var sayi in intDizi)
+            //{
+            //    if (sayi == 40 || sayi == 50)
+            //    {
+            //        continue;
+            //    }
+            //    if (sayi == 90)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine("Sayı Değeri = {0}", sayi);
+            //#region
+            //int[] carpimTablosuDizi = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //foreach (int item in carpimTablosuDizi)
+            //{
+            //    foreach (var item1 in carpimTablosuDizi)
+            //    {
+            //        Console.WriteLine("{0}x{1}={2}", item, item1, item * item1);
+            //        Console.WriteLine("***********");
+            //    }
+            //}
+
+            //#endregion
+        }
+    }
+}
+
